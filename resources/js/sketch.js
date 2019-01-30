633f5f96d791dc806b9ba9f0b443849da4b7893b
Sujeet Kumar <sujeetk@Virtusa.com>
2019-01-30 18:11:10 +0530

    adding floor and circles

--- resources/js/sketch.js
+++ resources/js/sketch.js
@@ -8,44 +8,64 @@ var Engine = Matter.Engine,
 var engine;
 var boxes = [];
 var world;
-var ground;
+var grounds = [];
 function setup() {
     createCanvas(400, 400);
     engine = Engine.create();
     world = engine.world;
-    // box1 = Bodies.rectangle(50, 100, 80, 80);
-    // ground = Bodies.rectangle(0, 300, 400, 10, { isStatic: true });
-    // World.add(world, box1);
     Engine.run(engine);
-    var options = {
-        isStatic: true
-    }
-    ground = Bodies.rectangle(200, height-50, width, 10, options);
-    World.add(world, ground);
+    grounds.push(new Ground(100, height / 2, width / 1.5, 10, 0.3));
+    grounds.push(new Ground(300, height * 3 / 4, width / 1.5, 10, -0.3));
 }
 
 function mouseDragged() {
-    boxes.push(new Box(mouseX, mouseY, 20, 20));
+    boxes.push(new Box(mouseX, mouseY, random(8, 25), 20));
 }
 
 function draw() {
     background(51)
-    // rect(box1.position.x, box1.position.y, 80, 80)
-    // rect(ground.position.x, ground.position.y, 400, 10)
     for (var bx = 0; bx < boxes.length; bx++) {
         boxes[bx].show();
     }
-    noStroke(255);
-    fill(170);
-    rectMode(CENTER);
-    rect(ground.position.x, ground.position.y, width, 10);
-    // box1.show();
+    for (var gd = 0; gd < grounds.length; gd++) {
+        grounds[gd].show();
+    }
 }
 
 function Box(x, y, w, h) {
     var options = {
-        friction: 0.5,
-        restitution: 1
+        friction: 0,
+        restitution: 0.6
+    }
+    // this.body = Bodies.rectangle(x, y, w, h, options);
+    this.body = Bodies.circle(x, y, w, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+    var r = random(0, 255);
+    var g = random(0, 255);
+    var b = random(0, 255);
+    this.show = function () {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        // rectMode(CENTER)
+        strokeWeight(2)
+        stroke(255)
+        fill(r, g, b);
+        ellipse(0, 0, 2 * w);
+        pop();
+    }
+}
+
+function Ground(x, y, w, h, a) {
+    var options = {
+        friction: 0.3,
+        restitution: 0.6,
+        isStatic: true,
+        angle: a
     }
     this.body = Bodies.rectangle(x, y, w, h, options);
     this.w = w;
@@ -54,14 +74,12 @@ function Box(x, y, w, h) {
 
     this.show = function () {
         var pos = this.body.position;
-        var angle = this.body.angle;
         push();
         translate(pos.x, pos.y);
-        rotate(angle);
+        rotate(a);
         rectMode(CENTER)
-        strokeWeight(2)
-        stroke(255)
-        fill(127)
+        noStroke(255)
+        fill(170)
         rect(0, 0, this.w, this.h);
         pop();
     }
